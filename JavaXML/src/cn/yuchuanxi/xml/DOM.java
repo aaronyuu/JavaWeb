7a993a3f3cbfd6318c0f92cea7a55772e2a75d06
aaronyuu <vcvok@qq.com>
2014-12-10 23:38:05 +0800

    整理DOM读取XML文件方法
    整理DOM读取XML文件方法

--- JavaXML/src/cn/yuchuanxi/xml/DOM.java
+++ JavaXML/src/cn/yuchuanxi/xml/DOM.java
@@ -13,58 +13,76 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class DOMTest {
+public class DOM {
 
 	public static void main(String[] args) {
+		//1.获取DocumentBuilderFactory
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
 				.newInstance();
 
 		try {
+			//2.获取DocumentBuilder
 			DocumentBuilder documentBuilder = documentBuilderFactory
 					.newDocumentBuilder();
+			//3.获取Document
 			Document doc = documentBuilder.parse("http://localhost:8080/YssCRM/books.xml");
-			// 通过getElementsByTagName方法,传入节点名获取节点列表
+			//a. 通过getElementsByTagName方法,传入节点名获取book节点列表
 			NodeList bookList = doc.getElementsByTagName("book");
-
+			//循环遍历节点列表
 			for (int i = 0; i < bookList.getLength(); i++) {
-				System.out.println("============下面开始遍历第" + (i + 1)
-						+ "本书的内容==========");
+				System.out.println("============下面开始遍历第" + (i + 1) + "本书的内容==========");
+				//通过NodeList.item(i)获取一个节点列表中的一个节点
 				Node book = bookList.item(i);
 //				System.out.println(book.getTextContent());
 				// System.out.println("节点名:"+book.getNodeName());
 				// System.out.println("\t节点属性:");
+				//获取节点属性列表
 				NamedNodeMap attr = book.getAttributes();
-				// System.out.println("第"+(i+1)+"本书,共有"+attr.getLength()+"个属性");
+				
+				/*
+				 * 获取属性方法一:
+				 */
+				//获取属性个数
+				 System.out.println("属性个数:"+attr.getLength());
+				 //遍历所有属性
+					for (int j = 0; j < attr.getLength(); j++) {
+						//获取属性名:
+						System.out.print("属性名"+attr.item(j).getNodeName() );
+						//获取属性值
+						System.out.println("\t属性值:"+ attr.item(j).getNodeValue());
+					}
 
-				// 如果已知属性名是id,则可通过下面方法获取属性值
+				/*
+				 * 获取属性值方法二:
+				 * 如果已知属性名是id,则可通过下面方法获取属性值
+				 */
 				Element bookE = (Element) bookList.item(i);
 				System.out.println("ID属性值是:" + bookE.getAttribute("id"));
 
-				for (int j = 0; j < attr.getLength(); j++) {
-					System.out.println(attr.item(j).getNodeName() + ":"
-							+ attr.item(j).getNodeValue());
-				}
+		
 
-				// 遍历book节点的子节点列表
+				/*
+				 * 获取book节点的子节点列表
+				 */
 				NodeList bookChildNodes = book.getChildNodes();
+				//遍历子结点.
 				for (int k = 0; k < bookChildNodes.getLength(); k++) {
-					// 获取book节点的子节点
+					// 获取book节点的单个子节点
 					Node bookChildNode = bookChildNodes.item(k);
+					// Node.ELEMENT_NODE 代表是个?
 					if (bookChildNode.getNodeType() == Node.ELEMENT_NODE) {
+						//获取节点名
 						System.out.print("节点名是:" + bookChildNode.getNodeName());
-						System.out.println("===>节点内容是:"
-								+ bookChildNode.getTextContent());
-						// System.out.println("===>节点类型是:"+bookChildNode.getNodeType());
+						//通过方法getTextContent()获取节点标签包裹的文字
+						System.out.print("===>节点内容是:"+ bookChildNode.getTextContent());
+						//获取节点类型
+						 System.out.println("===>节点类型是:"+bookChildNode.getNodeType());
 					}
 				}
 
 				System.out.println("============下面结束遍历第" + (i + 1)
-						+ "本书的内容==========");
+						+ "本书的内容==========\n");
 			}
-
-			// for (Node book : bookList) {
-			//
-			// }
 
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
